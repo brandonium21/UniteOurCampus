4e32124bce9058aa348c3a7182a39f0bf844264f
dontae1 <brandonium21@gmail.com>
2016-03-24 12:54:37 -0500

    created register

--- app/routes/index.py
+++ app/routes/index.py
@@ -1,5 +1,15 @@
 from app import app
+from flask import redirect, render_template, render_template_string, request, url_for, json
 
 @app.route('/')
 def root():
     return app.send_static_file('index.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def reg():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        print name
+        print email
+    return render_template('register.html')
