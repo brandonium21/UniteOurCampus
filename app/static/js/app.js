b5efd28fe27ccd2d3482eee9ac7c9481906bfa19
crunchcrackle <sarosh.6@gmail.com>
2016-04-05 18:07:41 -0500

    Added sponsor and team page, anjul to keynote slides, polished styling

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -14,8 +14,8 @@ angular.module('Unite our campus', ['ngResource', 'ngRoute', 'ui.bootstrap', 'ui
 $(document).ready(function() {
     $('#fullpage').fullpage({
         menu: '#menu',
-        sectionsColor: ['#122847', '#89cff0', '#FFFFFF', '#FA6300'],
-        anchors: ['firstpage', 'secondpage', 'thirdpage', 'fourthpage'],
+        sectionsColor: ['#122847', '#89cff0', '#FFFFFF', '#89cff0', '#122847', '#02898D'],
+        anchors: ['firstpage', 'secondpage', 'thirdpage', 'fourthpage', 'fifthpage', 'sixthpage'],
         css3: true,
         scrollingSpeed: 800,
         verticalCentered: true
